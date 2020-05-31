946c5a7efe4643e4e10efca01dd9a77a13679933
Yash Gaitonde <gaitonde@umich.edu>
2020-05-31 18:01:48 +0000

    pseudocode

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-nvers
+#pseudocode
+#sortAndCount(arr A, length n)
+# if n = 1 return 0
+# else 
+#   x = sortAndCount(A[:n/2], n/2)
+#   y = sortAndCount(A[n/2:], n/2)
+#   z = mergeAndCountSplit(a,n)
+# return x+y+z
+
+def sortAndCount(a, n):
+  if(n==1):
+    return 0;
+  else:
+    x = sortAndCount(a[:n/2], n/2)
+    y = sortAndCount(a[n/2:], n/2)
+    z = mergeAndCountSplit(a,n)
+    return x+y+z;
+
+
+def mergeAndCountSplit:
+  return 0;
