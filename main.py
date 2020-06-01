d56ecd859ce1a78c7fc65b87a79da93092a7f398
Yash Gaitonde <gaitonde@umich.edu>
2020-06-01 16:10:04 +0000

    changed merge logic

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 #sortAndCount(arr A, length n)
 # if n = 1 return 0
 # else 
-#   x = sortAndCount(A[:n/2], n/2)
+#   x = sortAndCount([A:n/2], n/2)
 #   y = sortAndCount(A[n/2:], n/2)
 #   z = mergeAndCountSplit(a,n)
 # return x+y+z
+invCount = 0
 
 def sortAndCount(a):
   n = len(a)
@@ -16,12 +17,19 @@ def sortAndCount(a):
   else:
     x = sortAndCount(left)
     y = sortAndCount(right)
-    z = mergeAndCountSplit(left, right)
+    z = mergeAndCountSplit(a, left, right)
     return x+y+z;
 
 
-def mergeAndCountSplit(left, right):
-  return 0;
+def mergeAndCountSplit(a, left, right):
+  #add logic if left != right
+  count, i, j = 0;
+  while(i<len(left) or j<len(right)):
+    if(left[i] > right[i]):
+      a[i] = left[i]
+    else: 
+      a[i] = right[i]
+    return count
 
 a = [6,5,4,3,2,1]
-print(sortAndCount(a))
+sortAndCount(a)
